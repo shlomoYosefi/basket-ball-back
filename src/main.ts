c0d4e8cca632a900f7a616ccce5f8fc17e5e951f
Shlomo Yosefi <y0584400886@gmail.com>
2020-12-15 13:37:47 +0200

    make it betterp

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
 
   await app.listen( process.env.PORT  || 3000);
-  console.log(process.env.PORT);
+  console.log("this port is",process.env.PORT);
   
 }
 bootstrap();
